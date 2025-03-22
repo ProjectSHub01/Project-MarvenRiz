e3e840d442f3ce13475ca9a668e7ce37e6b89dd2
ProjectSHub01 <oooiug27@gmail.com>
2025-03-23 01:12:18 +0700

    Update LoadstringUrl.lua

--- LoadstringUrl.lua
+++ LoadstringUrl.lua
@@ -1,20 +1,23 @@
-_G.Map = {
-  ["Information"] = {
-    Discord = "nil",
-    YouTube = "nil"
-  },
-  [90823899207715] = {
-    Url = "https://raw.githubusercontent.com/ProjectSHub01/Project-MarvenRiz/refs/heads/main/Arcane%20Piece.lua",
-    Status = "🔵 [Beta]"
-  }
-}
+local function Link()
+    local Map = {
+        ["Information"] = {
+            Discord = "nil",
+            YouTube = "nil"
+        },
+        [90823899207715] = {
+            Url = "https://raw.githubusercontent.com/ProjectSHub01/Project-MarvenRiz/refs/heads/main/Arcane%20Piece.lua",
+            Status = "🔵 [Beta]"
+        }
+    }
+    return Map
+end
 
-if _G.Map[game.PlaceId] then
-  pcall(function()
-    loadstring(game:HttpGet(_G.Map[game.PlaceId].Url))()
-  end)
+local Map = Link()
+
+if Map[game.PlaceId] then
+    pcall(function()
+        loadstring(game:HttpGet(Map[game.PlaceId].Url))()
+    end)
 else
     game.Players.LocalPlayer:Kick("Not Supported Map")
 end
-
-return _G.Map
